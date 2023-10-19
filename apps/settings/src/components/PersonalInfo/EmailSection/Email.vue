9ddb9b4a6255861d8d6c4ef135af72533335da35
Grigorii K. Shartsev <me@shgk.me>
2023-10-19 13:53:38 +0200

    fix(settings): remove empty aria-describedby
    Empty string `''` is still rendered and is not a valid ID.
    
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- apps/settings/src/components/PersonalInfo/EmailSection/Email.vue
+++ apps/settings/src/components/PersonalInfo/EmailSection/Email.vue
@@ -29,7 +29,7 @@
 				autocomplete="email"
 				:placeholder="inputPlaceholder"
 				:value="email"
-				:aria-describedby="helperText ? `${inputId}-helper-text` : ''"
+				:aria-describedby="helperText ? `${inputId}-helper-text` : undefined"
 				autocapitalize="none"
 				autocorrect="off"
 				@input="onEmailChange">
